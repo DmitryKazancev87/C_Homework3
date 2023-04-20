c3be7058b884733b064811d81b67000af0c8640d
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-04-20 23:16:37 +0300

    Черновик решения задачи 3

---
+++ Task 10/Program.cs
@@ -0,0 +1,10 @@
+﻿// 10. Программа принимающая на входе число N и выдающая таблицу кубов чисел от 1 до N.
+
+Console.WriteLine(" Введите число N : ");
+int N = Convert.ToInt32(Console.ReadLine());
+for (int i = 1; i<=N+1; i++)
+{
+    i = (i * i * i);
+    Console.Write($" {i} ");
+}
+Console.WriteLine($" : таблица кубов от 1 до {N}.");
