92b07cefbc16643a14d056f8cdd6d560d7bcd339
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-04-20 16:21:06 +0300

    Черновик первой задачи

---
+++ Task 8/Program.cs
@@ -0,0 +1,19 @@
+﻿// 8. Программа определяющая является ли пятизначное число палиндромом или нет.
+
+Console.WriteLine("Введите пятизначное число :");
+string N = Console.ReadLine();
+int Len = N.Length;
+    if (Len==5)
+    {
+        if (N[0]==N[4] && N[1]==N[3])
+        {
+            System.Console.WriteLine($:"{N} - полиндром");
+        }     
+        else {
+            System.Console.WriteLine($:"{N} - не полиндром");
+        }
+    }
+    else
+    {
+        System.Console.WriteLine($ "Ошибка : {N} - введено не пятезначное число");
+    }
