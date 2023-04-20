161650d93a2bed15f1ddf870df45f143d8aee49d
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-04-20 16:29:51 +0300

    Решение задачи 1

--- Task 8/Program.cs
+++ Task 8/Program.cs
@@ -3,17 +3,18 @@
 Console.WriteLine("Введите пятизначное число :");
 string N = Console.ReadLine();
 int Len = N.Length;
-    if (Len==5)
+if (Len == 5)
+{
+    if (N[0] == N[4] && N[1] == N[3])
     {
-        if (N[0]==N[4] && N[1]==N[3])
-        {
-            System.Console.WriteLine($:"{N} - полиндром");
-        }     
-        else {
-            System.Console.WriteLine($:"{N} - не полиндром");
-        }
+        Console.WriteLine($"{N} - полиндром.");
     }
     else
     {
-        System.Console.WriteLine($ "Ошибка : {N} - введено не пятезначное число");
+        Console.WriteLine($"{N} - не полиндром.");
     }
+}
+else
+{
+    Console.WriteLine($"Ошибка : {N} - введено не пятизначное число!");
+}
