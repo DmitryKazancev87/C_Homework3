36a52fce3534cf083218d8a6c644c3fa80c915a5
Dmitry Kazancev <kaz.freeman@bk.ru>
2023-04-20 23:44:48 +0300

    Решение задачи 3

--- Task 10/Program.cs
+++ Task 10/Program.cs
@@ -1,10 +1,22 @@
 ﻿// 10. Программа принимающая на входе число N и выдающая таблицу кубов чисел от 1 до N.
 
 Console.WriteLine(" Введите число N : ");
-int N = Convert.ToInt32(Console.ReadLine());
-for (int i = 1; i<=N+1; i++)
-{
-    i = (i * i * i);
-    Console.Write($" {i} ");
+int Number = int.Parse(Console.ReadLine());
+for (int i = 1; i <= Number; i++)
+{   
+    Console.Write($"{i*i*i}  ");
 }
-Console.WriteLine($" : таблица кубов от 1 до {N}.");
+Console.WriteLine($" : таблица кубов от 1 до {Number}.");
+
+
+// Ввод через фунцию EnterInformation:
+
+//int EnterInfomation (string message)
+//{
+//    Console.Write (message);
+//    return int.Parse (Console.ReadLine());
+//}
+//int Number = EnterInfomation(" Введите число N : ");
+//for (int i = 1; i <= Number; i++)
+//{
+//       Console.Write($"{i*i*i}  ");}
